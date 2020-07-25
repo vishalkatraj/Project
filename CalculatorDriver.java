6a14a12ce99aef11e774346eae80a7dbf6f11b42
vishalkatraj <vishalkatraj@gmail.com>
2020-07-25 15:35:34 +0930

    feature-z branch modify

--- CalculatorDriver.java
+++ CalculatorDriver.java
@@ -4,15 +4,14 @@ package guicalculator;
 import javax.swing.JFrame;
 
 /*
- * Driver Class for the Calcultor
+ * Driver Class
  *
- * @author 
+
  */
 public class CalculatorDriver {
 
-   /**
-    * @param args the command line arguments
-    */
+  
+    
    public static void main(String[] args) {
       JFrame frame = new JFrame("Simple Calculator");
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
